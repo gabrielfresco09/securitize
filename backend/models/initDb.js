ab0c15e88b5cbaa6077c86db94032de16adf8baf
Gabriel Fresco <gfresco@atixlabs.com>
2020-06-05 00:39:07 -0300

    Removed unused code from mongoose integration

--- backend/models/initDb.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connect = () =>
-  mongoose.connect(process.env.DATABASE_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-  });
-
-module.exports = connect;
